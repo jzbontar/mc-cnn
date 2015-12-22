fb205e0e22896ae2477053bfb686a982e56fc2c3
Jure Zbontar <jure.zbontar@gmail.com>
2015-12-22 15:59:13 +0100

    Nicer bin2png script.

--- samples/bin2png.lua
+++ samples/bin2png.lua
@@ -1,16 +1,20 @@
 require 'cutorch'
 require 'image'
 
+d = 70
+h = 370
+w = 1226
+
 print('Writing left.png')
-left = torch.FloatTensor(torch.FloatStorage('left.bin')):view(1, 70, 370, 1226):cuda()
+left = torch.FloatTensor(torch.FloatStorage('left.bin')):view(1, d, h, w):cuda()
 _, left_ = left:min(2)
-image.save('left.png', left_[1]:float():div(70))
+image.save('left.png', left_[1]:float():div(d))
 
 print('Writing right.png')
-right = torch.FloatTensor(torch.FloatStorage('right.bin')):view(1, 70, 370, 1226):cuda()
+right = torch.FloatTensor(torch.FloatStorage('right.bin')):view(1, d, h, w):cuda()
 _, right_ = right:min(2)
-image.save('right.png', right_[1]:float():div(70))
+image.save('right.png', right_[1]:float():div(d))
 
 print('Writing disp.png')
-disp = torch.FloatTensor(torch.FloatStorage('disp.bin')):view(1, 1, 370, 1226)
-image.save('disp.png', disp[1]:div(70))
+disp = torch.FloatTensor(torch.FloatStorage('disp.bin')):view(1, 1, h, w)
+image.save('disp.png', disp[1]:div(d))
