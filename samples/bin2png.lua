b944e2fb58b130a01ec70dd7314ed540e970f58a
Jure Zbontar <jure.zbontar@gmail.com>
2017-08-18 17:25:14 -0400

    Fix bin2png to work with new torch

--- samples/bin2png.lua
+++ samples/bin2png.lua
@@ -3,18 +3,26 @@ require 'image'
 
 d = 70
 h = 370
-w = 1226
+w= 1226
+
+function fromfile(fname)
+   local size = io.open(fname):seek('end')
+   local x = torch.FloatTensor(torch.FloatStorage(fname, false, size / 4))
+   local nan_mask = x:ne(x)
+   x[nan_mask] = 1e38
+   return x
+end
 
 print('Writing left.png')
-left = torch.FloatTensor(torch.FloatStorage('left.bin')):view(1, d, h, w):cuda()
+left = fromfile('left.bin'):view(1, d, h, w)
 _, left_ = left:min(2)
 image.save('left.png', left_[1]:float():div(d))
 
 print('Writing right.png')
-right = torch.FloatTensor(torch.FloatStorage('right.bin')):view(1, d, h, w):cuda()
+right = fromfile('right.bin'):view(1, d, h, w)
 _, right_ = right:min(2)
 image.save('right.png', right_[1]:float():div(d))
 
 print('Writing disp.png')
-disp = torch.FloatTensor(torch.FloatStorage('disp.bin')):view(1, 1, h, w)
+disp = fromfile('disp.bin'):view(1, 1, h, w)
 image.save('disp.png', disp[1]:div(d))
